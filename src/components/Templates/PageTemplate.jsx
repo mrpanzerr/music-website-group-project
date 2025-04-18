fa8d6943e81d07e073fac10928f13b27fb52f4a2
mrpanzerr <gaetanopanzer01@gmail.com>
2025-04-18 13:56:18 -0400

    more comments added and reordering of page template

--- src/components/Templates/PageTemplate.jsx
+++ src/components/Templates/PageTemplate.jsx
@@ -2,7 +2,7 @@
 Component: PageTemplate  
 Author: Ryan Ferrel
 Date Created: 04.10.2025  
-Last Revised: 04.14.2025  
+Last Revised: 04.18.2025  
 
 Purpose:  
 This React component dynamically renders a detailed view for either an artist, track, or album based on the data passed via props. It formats and displays various metadata such as images, names, genres, release dates, and associated items (e.g., top tracks for an artist or tracks in an album).
@@ -62,60 +62,132 @@ const PageTemplate = ({ data }) => {
     return (
         <div className="page-template">
             {type === "artist" && (
-                <ul>
-                    <li>Artist Name: {artistname}</li>
-                    <li>Artist's Spotify Followers: {followers}</li>
-                    <li><img src={artistimage}></img></li>
-                    <li>Artist's Genres<ul className="artistGenres">
-                        {genres.map((genre,index) => (
-                            <li key={index}>{genre}</li>
-                        ))}
-                    </ul></li>
-                    <li>Artist's Top Tracks: <ul> {toptracks.map((track) => (
-                        <ul>
-                        <a href = {`/track/${track[1]}`}><li><u>{track[0]}</u></li></a>
-                        <a href = {`/album/${track[3]}`}><li><img src={track[2]}></img></li></a>
-                        </ul>))}
-                    </ul></li>
-                </ul>
-            )}
+			  <section className="artistDetails flex flex-col gap-6">
+				{/* Artist Header */}
+				<div className="flex gap-4">
+				  <h1>{artistname}</h1>
+				  <div className="artistImage w-1/3 max-w-xs">
+					<img
+					  src={artistimage}
+					  alt={`${artistname}`}
+					  className="w-full h-auto rounded-xl shadow-md object-cover"
+					/>
+				  </div>
+				  <ul className="artistInfo flex-1 space-y-2">
+					<li><strong>Spotify Followers:</strong> {followers.toLocaleString()}</li>
+					{genres > 0 ? (
+						<li>
+							<ul className="artistGenres list-disc list-inside">
+							{genres.map((genre, index) => (
+							  <li key={index}>{genre}</li>
+							))}
+							</ul>
+						</li>
+					) : (
+						<h1></h1>
+					)}
+				  </ul>
+				</div>
 
+				{/* Top Tracks Section */}
+				<div className="artistTopTracks mt-6">
+				  <h2 className="text-lg font-semibold mb-2">Top Tracks</h2>
+				  <ul className="topTracks flex flex-col gap-3">
+					{toptracks.slice(0, 10).map(([trackName, trackId, albumImage, albumId], index) => (
+					  <li key={index} className="flex items-center gap-4 p-2 rounded-md hover:bg-gray-100 transition">
+						<a href={`/album/${albumId}`} className="w-12 h-12 flex-shrink-0">
+						  <img
+							src={albumImage}
+							alt={`${trackName} album cover`}
+							className="w-full h-full object-cover rounded"
+						  />
+						</a>
+						<a href={`/track/${trackId}`} className="text-blue-600 hover:underline">
+						  {trackName}
+						</a>
+					  </li>
+					))}
+				  </ul>
+				</div>
+
+				{/* <Thoughts /> -- This will be the posting/comment component */}
+			  </section>
+			)}
 
             {type === "track" && (
-                <ul>
-                    <li>Track Name: {trackname}</li>
-                    <a href={`/album/${trackalbumid}`}><li>Track Album Type: {trackalbumtype}</li>
-                    <li>Track Album Name: {trackalbumname}</li>
-                    <li><img src={trackimage}></img></li></a>
-                    <li>Track Artists<ul className="trackGenres">
-                        {trackartists.map((artist,index) => (
-                            <a href={`/artist/${artist[1]}`}><li key={index}>{artist[0]}</li></a>
-                        ))}
-                    </ul></li>
-                    <li>Track Release Date: {trackreleasedate}</li>
-                </ul>
-            )}
+			  <section className="trackDetails flex flex-col gap-4">
+				<div className="flex gap-4">
+				  <div className="trackImage w-1/3 max-w-xs">
+					<img
+					  src={trackimage}
+					  alt={`${trackname} cover`}
+					  className="w-full h-auto rounded-xl shadow-md"
+					/>
+				  </div>
+				  <ul className="trackInfo flex-1 space-y-2">
+					<li><strong>Track Name:</strong> {trackname}</li>
+					<li>
+					  <strong>Album:</strong>{' '}
+					  <a href={`/album/${trackalbumid}`} className="text-blue-600 hover:underline">
+						{trackalbumname}
+					  </a>
+					</li>
+					<li>
+					  <strong>Artists:</strong>
+					  <ul className="trackArtists list-disc list-inside">
+						{trackartists.map(([name, id], index) => (
+						  <li key={index}>
+							<a href={`/artist/${id}`} className="text-blue-600 hover:underline">
+							  {name}
+							</a>
+						  </li>
+						))}
+					  </ul>
+					</li>
+					<li><strong>Release Date:</strong> {trackreleasedate}</li>
+				  </ul>
+				</div>
 
+				{/* <Thoughts /> -- This will be the posting/comment component */}
+			  </section>
+			)}
 
             {type === "album" && (
-                <ul>
-                    <li>Name: {albumname}</li>
-                    <li>Album Type: {albumtype}</li>
-                    <li>Number of Tracks: {tracknumber}</li>
-                    <li><img src={albumimage}></img></li>
-                    <li>Album Release Date: {albumreleasedate}</li>
-                    <li>Artists: <ul className="albumArtists">
-                        {albumartists.map((artist,index) => (
-                            <li key={index}>{artist[0]}</li>
-                        ))}
-                    </ul></li>
-                    <li>Tracks: <ul className="albumTracks">
-                        {albumtracks.map((track,index) => (
-                            <a href={`/track/${track[1]}`}><li key={index}>{track[0]}</li></a>
-                        ))}
-                    </ul></li>
-                </ul>
-            )}
+				<section className="albumDetails flex flex-col gap-4">
+					<div className="flex gap-4">
+						<div className="albumImage w-1/3 max-w-xs">
+							<h1>{albumname}</h1>
+							<img src={albumimage} alt={`${albumname} cover`} className="w-full h-auto rounded-xl shadow-md" />
+						</div>
+						<ul className="albumInfo flex-1 space-y-2">
+							<li>
+								<strong>Artists:</strong>
+								<ul className="albumArtists list-disc list-inside">{albumartists.map(([name], index) => (
+									<li key={index}>{name}</li>
+								))}
+								</ul>
+							</li>
+							<li><strong>Number of Tracks:</strong> {tracknumber}</li>
+							<li><strong>Release Date:</strong> {albumreleasedate}</li>
+						</ul>
+					</div>
+
+					<div className="albumTracksSection mt-6">
+						<h2 className="text-lg font-semibold mb-2">Tracks</h2>
+						<ul className="albumTracks list-decimal list-inside space-y-1">
+							{albumtracks.map(([trackName, trackId], index) => (
+							<li key={index}>
+								<a href={`/track/${trackId}`} className="text-blue-600 hover:underline">
+								{trackName}
+								</a>
+							</li>
+							))}
+						</ul>
+					</div>
+
+					{/* <Thoughts /> -- This will be the posting/comment component */}
+				</section>
+			)};
         </div>
     );
 };
