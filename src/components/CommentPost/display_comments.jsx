5fbac309c7eb461520efe166a3f076cfc73cc3d0
LunarSleep <rsfarrell01@gmail.com>
2025-04-28 16:27:35 -0400

    minor changes

--- src/components/CommentPost/display_comments.jsx
+++ src/components/CommentPost/display_comments.jsx
@@ -5,10 +5,19 @@ const CommentDispay = () => {
     url_segments = full_url.split('/');
     last_segment = url_segments.pop()
     console.log(`Searching for comments under ${last_segment}`)
-    try {
-        fetch()
-    }
-    catch {
 
+        fetch("http://127.0.01:5000/createpost", {
+            method: "POST",
+            credentials: "include",
+            headers : {"Content-Type" : "application/json"},
+            body : JSON.stringify({last_segment}),
+        }).then(res => res.json()),then(
+            responseData => {
+                console.log(`Got comment data${responseData}`)
+            }
+        );
+    }
+
+        console.log("error occured when fetching comment data")
     }
 }
