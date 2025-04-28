ea5f60a9351b788d36cf9ffa36975741c69cf81c
msc5799 <msc5799@uncw.edu>
2025-04-28 15:46:57 -0400

    merging main into branch

---
+++ src/components/CommentPost/comment_form.jsx
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+
+export default function CreatePost() {
+
+    
+    const [status, setStatus] = useState('unclicked')
+    const [content, setContent] = useState('')
+    const openCommentForm = () => {
+        console.log(status)
+        setStatus('clicked');
+    }
+    
+    const submitCommentForm = () => {
+        const full_url = window.location.href;
+        const url_segments = full_url.split('/');
+        const last_segment = url_segments.pop();
+        fetch("http://127.0.01:5000/createpost", {
+            method: "POST",
+            credentials: "include",
+            headers : {"Content-Type" : "application/json"},
+            body : JSON.stringify({content, last_segment}),
+        });
+        console.log(`Attempting to submit ${content,last_segment}`);
+        setStatus('unclicked');
+        setContent('');
+    }
+
+    if (status === 'unclicked') {
+        return (<div><button onClick={openCommentForm}>Create Comment</button></div>)
+    }
+    if (status === 'clicked') {
+        return (<div>
+                    <input type="text" id="content" placeholder="Enter Comment" value={content} onChange={(e) => setContent(e.target.value)}></input>
+                    <button id="submit" onClick={submitCommentForm}>Submit</button>
+                </div>)
+    }
+}
