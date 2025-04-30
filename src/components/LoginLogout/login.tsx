a6c7d5d6414243e8626e0e748f7d568173c6af10
mrpanzerr <gaetanopanzer01@gmail.com>
2025-04-30 09:21:27 -0400

    fixed session

--- src/components/LoginLogout/login.tsx
+++ src/components/LoginLogout/login.tsx
@@ -98,6 +98,7 @@ function Login() {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ email, password }),
+		credentials: 'include' 
       });
 
       const data = await response.json();
